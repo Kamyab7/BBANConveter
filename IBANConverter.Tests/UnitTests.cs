e8159e1376dd35d27b19c84ee11cc4a863a5265d
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-19 21:54:00 +0330

    Add more test case

--- IBANConverter.Tests/UnitTests.cs
+++ IBANConverter.Tests/UnitTests.cs
@@ -11,6 +11,8 @@ namespace IBANConverter.Tests
 
         [Test]
         [TestCase("3120059467089", "015", AccountType.Seporde, "IR", "IR220150000003120059467089")]
+        [TestCase("611828005118352001", "056", AccountType.Seporde, "IR", "IR730560611828005118352001")]
+        [TestCase("020000008490224981", "012", AccountType.Seporde, "IR", "IR330120020000008490224981")]
         public void Test1(string accountNumber, string bankId, AccountType accountType, string countryCode,string expectedIBANValue)
         {
             var iban = new IBAN(accountNumber, bankId, accountType, countryCode);
