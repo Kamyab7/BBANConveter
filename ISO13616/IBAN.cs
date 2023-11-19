9aaa3f9cb2a3530eb257a4c82be15a1f8a427db1
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-19 20:27:08 +0330

    Add IBAN

---
+++ ISO13616/IBAN.cs
@@ -0,0 +1,123 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISO13616;
+
+public class IBAN
+{
+    /// <summary>
+    /// Country Code
+    /// </summary>
+    [StringLength(2)]
+    public string? CC { get; set; }
+
+
+    /// <summary>
+    /// Check Digit
+    /// </summary>
+    [StringLength(2)]
+    public string? CD => CalculateCD();
+
+
+    /// <summary>
+    /// Basic Bank Account Number
+    /// </summary>
+    [StringLength(22)]
+    public string? BBAN => CalculateBBAN();
+
+
+    /// <summary>
+    /// International Bank Account Number
+    /// </summary>
+    [StringLength(26)]
+    public string? Value => CalculateIBAN();
+
+
+    /// <summary>
+    /// Bank Account Number
+    /// </summary>
+    public string AccountNumber { get; set; }
+
+    /// <summary>
+    /// Bank Id
+    /// </summary>
+    public string BankId { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public AccountType AccountType { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Zeros => CalculateZeros();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="accountNumber"></param>
+    /// <param name="bankId"></param>
+    /// <param name="accountType"></param>
+    public IBAN(string accountNumber, string bankId, AccountType accountType, string countryCode)
+    {
+        AccountNumber = accountNumber;
+
+        BankId = bankId;
+
+        AccountType = accountType;
+
+        CC = countryCode;
+    }
+
+    public bool IsValid => ValidateIBAN();
+
+    private bool ValidateIBAN()
+    {
+        throw new NotImplementedException();
+    }
+
+    private string CalculateIBAN()
+    {
+        return $"{CC}{CD}{BBAN}";
+    }
+
+    private string CalculateCD()
+    {
+        string temp = $"{BBAN}" + "182700"; // IR00
+        char[] a = temp.ToCharArray();
+        int r = 0;
+
+        foreach (char v in a)
+        {
+            r = (((r * 10) + int.Parse(v.ToString())) % 97);
+        }
+
+        int cd = 98 - r;
+
+        return cd.ToString();
+    }
+
+    private string CalculateBBAN()
+    {
+        return $"{BankId}{(int)AccountType}{Zeros}{AccountNumber}";
+    }
+
+    private string CalculateZeros()
+    {
+        string zeros = String.Empty;
+        int numberOfZeros = 18 - AccountNumber.Length;
+
+        for (int i = 0; i < numberOfZeros; i++)
+        {
+            zeros += "0";
+        }
+
+        return zeros;
+    }
+}
+
+public enum AccountType
+{
+    Seporde,
+    Tashilat
+}
--- ISO13616/Program.cs
+++ ISO13616/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ISO13616;
+
+var iban = new IBAN("3120059467089", "015", AccountType.Seporde, "IR");
+
+Console.WriteLine(iban.Value);
+
+Console.ReadLine();
