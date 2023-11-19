facc14521ac309c0b90e7808e8ad5fa5b76b0d4c
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-19 22:01:23 +0330

    Improve tests

--- IBANConverter.Tests/UnitTests.cs
+++ IBANConverter.Tests/UnitTests.cs
@@ -10,16 +10,17 @@ namespace IBANConverter.Tests
         }
 
         [Test]
-        [TestCase("3120059467089", "015", AccountType.Seporde, "IR", "IR220150000003120059467089")]
-        [TestCase("611828005118352001", "056", AccountType.Seporde, "IR", "IR730560611828005118352001")]
-        [TestCase("020000008490224981", "012", AccountType.Seporde, "IR", "IR330120020000008490224981")]
-        public void Test1(string accountNumber, string bankId, AccountType accountType, string countryCode,string expectedIBANValue)
+        [TestCase("3120059467089", "015", AccountType.Seporde, "IR", "IR220150000003120059467089","22")]
+        [TestCase("611828005118352001", "056", AccountType.Seporde, "IR", "IR730560611828005118352001","73")]
+        [TestCase("020000008490224981", "012", AccountType.Seporde, "IR", "IR330120020000008490224981","33")]
+        public void Test1(string accountNumber, string bankId, AccountType accountType, string countryCode,string expectedIBANValue,string expectedCD)
         {
             var iban = new IBAN(accountNumber, bankId, accountType, countryCode);
 
             iban.AccountNumber.Should().Be(accountNumber);
             iban.AccountType.Should().Be(accountType);
             iban.CC.Should().Be(countryCode);
+            iban.CD.Should().Be(expectedCD);
             iban.Value.Should().Be(expectedIBANValue);
         }
     }
