4bf4d4110fbb306f4253963545cdb90605cac904
Kamyab7 <valipourkamyab.official@gmail.com>
2023-11-19 21:16:04 +0330

    Make CC dynamic

--- ISO13616/IBAN.cs
+++ ISO13616/IBAN.cs
@@ -83,7 +83,37 @@ public class IBAN
 
     private string CalculateCD()
     {
-        string temp = $"{BBAN}" + "182700"; // IR00
+        Dictionary<char, int> dic = new Dictionary<char, int>()
+        {
+            ['A'] = 10,
+            ['B'] = 11,
+            ['C'] = 12,
+            ['D'] = 13,
+            ['E'] = 14,
+            ['F'] = 15,
+            ['G'] = 16,
+            ['H'] = 17,
+            ['I'] = 18,
+            ['J'] = 19,
+            ['K'] = 20,
+            ['L'] = 21,
+            ['M'] = 22,
+            ['N'] = 23,
+            ['O'] = 24,
+            ['P'] = 25,
+            ['Q'] = 26,
+            ['R'] = 27,
+            ['S'] = 28,
+            ['T'] = 29,
+            ['T'] = 30,
+            ['T'] = 31,
+            ['W'] = 32,
+            ['X'] = 33,
+            ['Y'] = 34,
+            ['Z'] = 35,
+        };
+
+        string temp = $"{BBAN}{dic[CC[0]]}{dic[CC[1]]}" + "00";
         char[] a = temp.ToCharArray();
         int r = 0;
 
@@ -119,5 +149,5 @@ public class IBAN
 public enum AccountType
 {
     Seporde,
-    Tashilat
+    Tashilat,
 }
--- ISO13616/Program.cs
+++ ISO13616/Program.cs
@@ -1,7 +1,15 @@
 ﻿using ISO13616;
 
-var iban = new IBAN("3120059467089", "015", AccountType.Seporde, "IR");
+var ibanSepah = new IBAN("3120059467089", "015", AccountType.Seporde, "IR");
+
+var iban = new IBAN("8480771708", "012", AccountType.Seporde, "IR");
+
+var iban2 = new IBAN("020000005236350887", "012", AccountType.Seporde, "IR");
+
+Console.WriteLine(ibanSepah.Value);
 
 Console.WriteLine(iban.Value);
+
+Console.WriteLine(iban2.Value);
 
 Console.ReadLine();
